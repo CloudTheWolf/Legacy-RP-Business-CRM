37a742e7822206f177f03eb3e4e1e710faebca65
CloudTheWolf <ukkmmichael@gmail.com>
2023-06-29 08:44:44 +0100

    reduce API timeout

--- app/Http/Controllers/Mechanic/Repair/RepairLogger.php
+++ app/Http/Controllers/Mechanic/Repair/RepairLogger.php
@@ -17,7 +17,7 @@ class RepairLogger extends Controller
     public function Get()
     {
         try {
-            $client = new Client(['base_uri' => env("API_BASE_URI"),'timeout' => 60]);
+            $client = new Client(['base_uri' => env("API_BASE_URI"),'timeout' => 5]);
             $response = $client->request('GET', '/op-framework/vehicles.json');
             $vehicles = json_decode($response->getBody());
         }
