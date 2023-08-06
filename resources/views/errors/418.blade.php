b3a57aecde546f2ca914d88b9c1b8cd467a1d067
CloudTheWolf <ukkmmichael@gmail.com>
2023-08-06 18:52:36 +0100

    Add errors

---
+++ resources/views/errors/418.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('I\'m a Teapot'))
+@section('code', '419')
+@section('message', __('I\'m a Teapot... I mean im in maintenance... try again shortly'))
